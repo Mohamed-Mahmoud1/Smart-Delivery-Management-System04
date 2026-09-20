b207662375cd20a06920776ce8e1b789ab225a04
Mohamed-Mahmoud1 <mohamedzanaty.dev@gmail.com>
2026-09-20 23:39:20 +0300

    Part 01 — Theoretical Questions-Question01

--- Smart Delivery Management System04/Program.cs
+++ Smart Delivery Management System04/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 — Theoretical Questions
+
+
+            #region Question01
+
+            //a) What is Abstraction in Object - Oriented Programming ?
+            //Answer:Abstraction means hiding implementation details and showing only essential features to the user, focusing on what an object does, not how it works.
+
+            //b)  Why is abstraction considered one of the four pillars of OOP?
+            //Answer:Because it provides maintainability, hides complex details, and shows essential information only.
+            #endregion
+
+
+            #endregion
         }
     }
 }
